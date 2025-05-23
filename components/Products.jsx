a134dded995d98dff34258e85c82935c9c077fad
Bryan Hoda <bryanhoda2003@gmail.com>
2025-05-23 18:07:17 +0530

    added filtering logic

--- components/Products.jsx
+++ components/Products.jsx
@@ -36,7 +36,7 @@ const Products = ({ selectedCategory, maxPrice }) => {
             <div className="products w-full md:w-[80%] min-h-[50vh]">
                 <h2 className="text-xl font-semibold">Product Listing</h2>
 
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 py-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-4">
 
                     {filteredProducts.length > 0 ? (
                         filteredProducts.map(product => (
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,8 +1,9 @@
 "use client"
 
 import { useState } from "react"
+import { RxCross2 } from "react-icons/rx";
 
-const Sidebar = ({ selectedCategory, setSelectedCategory }) => {
+const Sidebar = ({ selectedCategory, setSelectedCategory, sidebar, setSidebar }) => {
     const [price, setPrice] = useState(0);
 
     const handleCategoryChange = (e) => {
@@ -16,15 +17,16 @@ const Sidebar = ({ selectedCategory, setSelectedCategory }) => {
 
     return (
         <>
-            <div className="sidebar max-md:hidden min-w-[20%] min-h-[50vh] sticky top-0">
-                <div className="filters px-5 py-5 flex flex-col bg-blue-800 rounded-lg text-white">
+            <div className={`sidebar min-h-[50vh] md:!block md:sticky md:top-0 ${sidebar ? "block" : "hidden"} md:w-[20%] max-md:absolute max-md:z-10 w-[90%]`}>
+                <div className="filters relative px-5 py-5 flex flex-col bg-blue-800 rounded-lg text-white">
+                    <RxCross2 onClick={() => setSidebar(false)} className="absolute z-10 right-3 max-md:block hidden" size={16} />
                     <h3 className="text-lg font-semibold pb-3">Filters</h3>
                     <div className="category mb-3 flex flex-col gap-2">
                         <h3 className="py-2">Category</h3>
 
                         {["electronics", "jewelery", "men's clothing", "women's clothing", 'all'].map((cat) => (
-                            <label key={cat} className="flex items-center gap-2 mb-1 capitalize">
-                                <input type="radio" name="category" value={cat} checked={selectedCategory === cat || (cat === 'all' && selectedCategory === '')} onChange={handleCategoryChange} className="accent-blue-600 cursor-pointer" />
+                            <label key={cat} className="flex cursor-pointer w-fit items-center gap-2 mb-1 capitalize">
+                                <input type="radio" name="category" value={cat} onChange={handleCategoryChange} className="accent-blue-600 cursor-pointer" />
                                 {cat}
                             </label>
                         ))}
